9f70dc71c38ff4b705d8ce8859dbf22779bb969e
boricsk <borics.krisztian@gmail.com>
2024-10-31 12:52:15 +0100

    241031

--- 020 OOP create class/Program.cs
+++ 020 OOP create class/Program.cs
@@ -1,10 +1,13 @@
-﻿namespace _020_OOP_create_class
+﻿using System.Drawing;
+
+namespace _020_OOP_create_class
 {
     internal class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
+            
         }
     }
 }
